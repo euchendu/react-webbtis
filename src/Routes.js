913cbacb88d01265c6bae5b5f5dafc1295c436ee
euchendu <emekaguchendu@gmail.com>
2019-07-05 23:23:21 +0700

    add Not Found page

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
+import NotFound from './containers/NotFound';
 
 export default ({ childProps }) => (
-  <Route path="/" exact component={Home} />
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route component={NotFound} />
+  </Switch>
 );

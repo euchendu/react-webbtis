c3d2e7e8eff1766fa23a02ab0847b707a72326e1
euchendu <emekaguchendu@gmail.com>
2019-07-06 00:28:26 +0700

    add top nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import { withRouter } from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import './App.css';
 import Routes from './Routes';
 
@@ -7,6 +9,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Webbtis</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <Fragment>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </Fragment>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <Routes />
       </div>
     );

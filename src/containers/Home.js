6855212426e1eb5c064b785a602ba54901f759a9
euchendu <emekaguchendu@gmail.com>
2019-07-05 23:20:10 +0700

    add search form

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './Home.css';
+import {
+  FormGroup,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
 
 class Home extends Component {
   renderLander() {
@@ -8,6 +13,14 @@ class Home extends Component {
       <div className="lander">
         <h1>Webbtis</h1>
         <p>Web Based Book Tracking Information System</p>
+        <div>
+          <form>
+            <FormGroup controlId="keyword" size="lg">
+              <FormControl autoFocus type="text" placeholder="Enter title, author or ISBN" />
+            </FormGroup>
+            <Button size="sm" type="submit">Search</Button>
+          </form>
+        </div>
       </div>
     );
   }

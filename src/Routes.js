6d5eb5ffbacdfa52b970ae31069e365fdc50d54a
euchendu <emekaguchendu@gmail.com>
2019-07-07 12:39:45 +0700

    login with React and Firebase

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import SignUpPage from './containers/Signup';
+import LoginPage from './containers/Login';
 
 export default ({ childProps }) => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/signup" exact component={SignUpPage} />
+    <Route path="/login" exact component={LoginPage} />
     <Route component={NotFound} />
   </Switch>
 );
---
+++ src/containers/Login.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel,
+} from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import LoaderButton from '../components/LoaderButton';
+import './Login.css';
+import { FirebaseContext } from '../components/Firebase';
+
+const LoginPage = () => (
+  <div className="Login">
+    <FirebaseContext.Consumer>
+      {firebase => <LoginForm firebase={firebase} />}
+    </FirebaseContext.Consumer>
+  </div>
+);
+
+
+const INITIAL_STATE = {
+  isLoading: false,
+  email: '',
+  password: '',
+  error: null,
+};
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+  
+  login(email, password) {
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
+
+  validateForm() {
+    return this.state.email.length > 0
+      && this.state.password.length > 0;
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({ isLoading: true });
+    this.setState({ error: null });
+
+    try {
+      await this.login(this.state.email, this.state.password);
+    }
+    catch(e) {
+      alert(e);
+    }
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange} />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password" />
+        </FormGroup>
+        {this.state.error && <p>{this.state.error.message}</p>}
+        <LoaderButton
+          block
+          bsSize="large"
+          disabled={ ! this.validateForm() }
+          type="submit"
+          isLoading={this.state.isLoading}
+          text="Login"
+          loadingText="Logging in…" />
+      </form>
+    );
+  }
+}
+
+export default withRouter(LoginPage);
+
+export { LoginForm };
